430d99f8a8293741fffddf3d95c3296f79926477
Fabien Amann <fabien@feub.net>
2025-03-27 15:34:03 +0100

    refactor: added component ReleaseListItem.tsx

--- app/(tabs)/releases.tsx
+++ app/(tabs)/releases.tsx
@@ -6,6 +6,7 @@ import { API_URL } from "@env";
 import { ListReleasesType } from "@/types/releaseTypes";
 import MyText from "@/components/MyText";
 import { FlatList } from "react-native-gesture-handler";
+import ReleaseListItem from "@/components/ReleaseListItem";
 
 export default function Releases() {
   const [releases, setReleases] = useState<ListReleasesType[]>([]);
@@ -48,39 +49,7 @@ export default function Releases() {
       <View style={styles.container}>
         <FlatList
           data={releases}
-          renderItem={({ item }) => (
-            <View key={item.id.toString()} style={styles.resultItemContainer}>
-              <View style={styles.itemLeft}>
-                {item.artists && item.artists.length > 0 && (
-                  <>
-                    <MyText style={styles.dataText}>
-                      Artist(s):{" "}
-                      {item.artists?.map((artist) => artist.name).join(", ")}
-                    </MyText>
-                  </>
-                )}
-                <MyText style={styles.dataText}>{item.title}</MyText>
-                <MyText style={styles.dataText}>
-                  Date: {item.release_date}
-                </MyText>
-              </View>
-              {item.cover ? (
-                <Image
-                  source={
-                    typeof item.cover === "string"
-                      ? { uri: item.cover }
-                      : undefined
-                  }
-                  style={styles.image}
-                  resizeMode="cover"
-                />
-              ) : (
-                <View style={styles.mockImageContainer}>
-                  <MyText style={styles.mockImageText}>No cover</MyText>
-                </View>
-              )}
-            </View>
-          )}
+          renderItem={({ item }) => <ReleaseListItem release={item} />}
           keyExtractor={(releases) => releases.id.toString()}
           style={styles.entriesContainer}
         />
@@ -96,42 +65,11 @@ const styles = StyleSheet.create({
     color: "white",
     alignItems: "center",
     paddingVertical: 10,
+    paddingBottom: 25,
   },
   entriesContainer: {
     width: "100%",
     padding: 14,
     maxWidth: 600,
-  },
-  resultItemContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    color: "#f1f1f1",
-    backgroundColor: "#3f3f46",
-    marginBottom: 10,
-    padding: 8,
-    borderRadius: 10,
-  },
-  itemLeft: {
-    flex: 1,
-    flexShrink: 1,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 6,
-  },
-  mockImageContainer: {
-    width: 100,
-    height: 100,
-    borderRadius: 6,
-    backgroundColor: "#52525b",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  mockImageText: {
-    color: "#71717a",
-  },
-  dataText: {
-    color: "#f1f1f1",
   },
 });
---
+++ components/ReleaseListItem.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Image, View, StyleSheet, Alert } from "react-native";
+import { ListReleasesType } from "@/types/releaseTypes";
+import MyText from "@/components/MyText";
+
+function ReleaseListItem({ release }: { release: ListReleasesType }) {
+  return (
+    <View key={release.id.toString()} style={styles.resultItemContainer}>
+      <View style={styles.itemLeft}>
+        {release.artists && release.artists.length > 0 && (
+          <>
+            <MyText style={styles.dataTextArtist}>
+              {release.artists?.map((artist) => artist.name).join(", ")}
+            </MyText>
+          </>
+        )}
+        <MyText style={styles.dataText}>{release.title}</MyText>
+        <MyText style={styles.dataText}>Date: {release.release_date}</MyText>
+      </View>
+      {release.cover ? (
+        <Image
+          source={
+            typeof release.cover === "string"
+              ? { uri: release.cover }
+              : undefined
+          }
+          style={styles.image}
+          resizeMode="cover"
+        />
+      ) : (
+        <View style={styles.mockImageContainer}>
+          <MyText style={styles.mockImageText}>No cover</MyText>
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default ReleaseListItem;
+
+const styles = StyleSheet.create({
+  resultItemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    color: "#f1f1f1",
+    backgroundColor: "#3f3f46",
+    marginBottom: 10,
+    padding: 8,
+    borderRadius: 10,
+  },
+  itemLeft: {
+    flex: 1,
+    flexShrink: 1,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 6,
+  },
+  mockImageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 6,
+    backgroundColor: "#52525b",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mockImageText: {
+    color: "#71717a",
+  },
+  dataTextArtist: {
+    color: "#f1f1f1",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  dataText: {
+    color: "#f1f1f1",
+  },
+});
