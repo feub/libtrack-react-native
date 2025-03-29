cd38e76607c0b7b16289b228c51db1d617692499
Fabien Amann <fabien@feub.net>
2025-03-29 10:20:24 +0100

    refactor: got the StatusBar set to light

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout() {
         />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
     </>
   );
 }
