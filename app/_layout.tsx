95ae725707ac96fe146300c0bc6ab01bd7d5e00b
Fabien Amann <fabien@feub.net>
2025-05-07 14:59:59 +0200

    chore: (tabs)/_layout.tsx styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ Colors.loadColors({
 Typography.loadTypographies({
   h1: { fontSize: 58, fontWeight: "300", lineHeight: 80 },
   h2: { fontSize: 46, fontWeight: "300", lineHeight: 64 },
+  h3: { fontSize: 34, fontWeight: "300", lineHeight: 48 },
   body: { fontSize: 16 },
 });
 
