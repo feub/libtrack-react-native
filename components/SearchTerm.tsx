afd1518eced37ca9b92597733c2bc8768118bf2f
Fabien Amann <fabien@feub.net>
2025-05-07 16:46:56 +0200

    style: search text field border

--- components/SearchTerm.tsx
+++ components/SearchTerm.tsx
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
   },
   fieldStyle: {
     backgroundColor: Colors.background,
-    borderColor: Colors.text,
+    borderColor: "#6E7881",
     borderWidth: 1,
     borderRadius: 8,
     paddingHorizontal: Spacings.s2,
